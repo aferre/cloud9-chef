a41b0411f7d2bb03b8f74bfc12d73e37f2b76caf
Adrien Ferre <ferre.adrien@gmail.com>
2013-08-07 10:11:18 +0200

    added some metadata information

--- metadata.rb
+++ metadata.rb
@@ -1,6 +1,14 @@
-maintainer       "Example Com"
-maintainer_email "ops@example.com"
+name             "cloud9"
+maintainer       "Adrien Ferré"
+maintainer_email "ferre.adrien@gmail.com"
 license          "Apache 2.0"
 description      "Installs/Configures cloud9"
-
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.1"
+
+
+%w{ debian ubuntu }.each do |os|
+  supports os
+end
+
+depends "nvm"
