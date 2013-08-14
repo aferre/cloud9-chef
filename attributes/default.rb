ce8afc041ce1f3dc78b0465d20ec8b4c6d44de5a
Adrien Ferre <ferre.adrien@gmail.com>
2013-08-14 10:16:15 +0200

    Moved to last version instead of head for cloud9 git

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,6 +1,7 @@
 default['cloud9']['directory'] = '/usr/local/src/cloud9'
 default['cloud9']['repository'] = 'https://github.com/ajaxorg/cloud9.git'
 default['cloud9']['reference'] = 'master'
+default['cloud9']['revision'] = 'v2.0.93'
 default['cloud9']['nvm']['version'] = 'v0.8.25'
-default['nvm']['install_from_sources'] = true
+default['nvm']['install_from_sources'] = false
 default['nvm']['alias_as_default'] = true
