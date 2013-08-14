568633a96183f2eaeb4c80c33ed3c80edd92e79c
Adrien Ferre <adrien.ferre@g2mobility.com>
2013-08-14 11:25:26 +0200

    added user support and run cloud9 at the end

--- recipes/default.rb
+++ recipes/default.rb
@@ -47,6 +47,7 @@ nvm_install node['cloud9']['nvm']['version']  do
     alias_as_default node['nvm']['alias_as_default']
     action :create
     notifies :run, "bash[install_sourcemint]"
+    user node['cloud9']['user']
 end
 
 bash "install_sourcemint" do
@@ -58,6 +59,7 @@ bash "install_sourcemint" do
   EOH
 	ignore_failure false
 	action :nothing
+  user node['cloud9']['user']
 end
 
 git node['cloud9']['directory'] do
@@ -66,18 +68,32 @@ git node['cloud9']['directory'] do
   ignore_failure false
   action :sync
   notifies :run, "bash[install_cloud9]"
+  user node['cloud9']['user']
 end
-
 
 bash "install_cloud9" do
    code <<-EOH
   #{node['nvm']['source']}
   nvm use #{node['cloud9']['nvm']['version']}
   #{node['nvm']['directory']}/#{node['cloud9']['nvm']['version']}/lib/node_modules/sm/bin/sm install
+  chown -R www-data:www-data #{node['cloud9']['directory']}
   EOH
   cwd "#{node['cloud9']['directory']}"
   ignore_failure false
   action :nothing
-retries 2
+  retries 2
+  user node['cloud9']['user']
+  notifies :run, "bash[run_cloud9]"
 end
 
+bash "run_cloud9" do
+   code <<-EOH
+  #{node['nvm']['source']}
+  nvm use #{node['cloud9']['nvm']['version']}
+  #{node['cloud9']['directory']}/bin/cloud9.sh -l 0.0.0.0 -w /var/www
+  EOH
+  cwd "#{node['cloud9']['directory']}"
+  ignore_failure false
+  action :nothing
+  user node['cloud9']['user']
+end
