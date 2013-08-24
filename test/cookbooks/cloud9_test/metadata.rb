ec3502cc4ef56e33c6a1609a64ceb30252894740
Adrien Ferre <adrien.ferre@g2mobility.com>
2013-08-24 14:07:07 +0200

    added test cookbook

---
+++ test/cookbooks/cloud9_test/metadata.rb
@@ -0,0 +1,8 @@
+maintainer       "Adrien Ferre"
+maintainer_email "ferre.adrien@gmail.com"
+license          "Apache 2.0"
+description      "Installs/Configures cloud9_test"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          "0.0.1"
+
+depends "cloud9"
